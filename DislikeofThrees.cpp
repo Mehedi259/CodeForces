a952a80a50fe2cf6d53843fc227117aded9e32c8
Mehedi Hasan Mridul <mmridul207@gmail.com>
2023-05-28 20:09:20 +0600

    dislike of three

---
+++ DislikeofThrees.cpp
@@ -0,0 +1,53 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define fast() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int k;
+        cin >> k;
+
+        for (int i = 1;; i++)
+        {
+            if (i % 3 == 0 || i % 10 == 3)
+                continue;
+            if (--k == 0)
+            {
+                cout << i << '\n';
+                break;
+            }
+        }
+    }
+}
+int main()
+{
+    fast();
+
+    solve();
+
+    r0;
+}
