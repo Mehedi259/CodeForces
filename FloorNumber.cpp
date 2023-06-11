4e4c4dc754d46c94763a8d948326273378ad3f04
Mehedi Hasan Mridul <mmridul207@gmail.com>
2023-06-11 22:57:29 +0600

    floor number

---
+++ FloorNumber.cpp
@@ -0,0 +1,47 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define fast() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    if (n <= 2)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << ((n - 3) / x) + 2 << endl;
+    }
+}
+int main()
+{
+    fast();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    r0;
+}
