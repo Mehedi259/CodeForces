32ed640c4453e9037384a849afb503ab3b3beee9
Mehedi Hasan Mridul <mmridul207@gmail.com>
2023-06-11 18:47:20 +0600

    new year number

---
+++ NewYearsNumber.cpp
@@ -0,0 +1,50 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+#include <utility>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define fast() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int n;
+    cin >> n;
+    int y21 = n % 2020;
+    int y20 = (n - y21) / 2020 - y21;
+    if (y20 >= 0 && 2020 * y20 + 2021 * y21 == n)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+int main()
+{
+    fast();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    r0;
+}
