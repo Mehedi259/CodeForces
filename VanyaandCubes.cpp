7a1da055f134addc48690e492d42f4817c354c58
Mehedi Hasan Mridul <mmridul207@gmail.com>
2023-06-12 10:15:09 +0600

    Vanaya and cubes

---
+++ VanyaandCubes.cpp
@@ -0,0 +1,43 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define fast() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int n, l=0, cnt=0;
+    cin >> n;
+    while (cnt <= n)
+    {
+        l++;
+        cnt += (l*(l+1))/2;
+    }
+    
+    cout << l-1 << endl;
+}
+int main()
+{
+    fast();
+
+    solve();
+
+    r0;
+}
