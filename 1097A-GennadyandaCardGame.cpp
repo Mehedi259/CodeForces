5071dc3565d6e34fcb30e04e6ddf1938ad3b2033
Mehedi Hasan Mridul <mmridul207@gmail.com>
2023-06-26 09:36:50 +0600

    Genady and card game

---
+++ 1097A-GennadyandaCardGame.cpp
@@ -0,0 +1,46 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define fast() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+int solve()
+{
+    string s, x;
+    cin >> s;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> x;
+        if (s[0] == x[0] || s[1] == x[1])
+        {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+}
+int main()
+{
+    fast();
+
+    solve();
+
+    r0;
+}
