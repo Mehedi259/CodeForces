d8023c267a2fdc1ab766a77b9d95bd8441dbbb19
Mehedi Hasan Mridul <mmridul207@gmail.com>
2023-06-30 16:12:37 +0600

    1188A-VustheCossackandaContest.cpp

---
+++ 1188A-VustheCossackandaContest.cpp
@@ -0,0 +1,40 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define Mehedi_hasan() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+   int n,m,k;
+   cin>>n>>m>>k;
+   if(m>=n && k>=n) cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+}
+int main()
+{
+    Mehedi_hasan();
+
+    solve();
+
+    r0;
+}
---
+++ 1385A-ThreePairwiseMaximums.cpp
@@ -0,0 +1,44 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0 
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define Mehedi_hasan() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    vi a(3);
+    for(auto &it : a) cin >> it;
+    sort(all(a));
+    if(a[1] != a[2]) cout << "NO" << endl;
+    else cout << "YES" << endl << a[0] << " "  << a[0] << " " << a[2] << endl;
+}
+int main()
+{
+    Mehedi_hasan();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    r0;
+}
