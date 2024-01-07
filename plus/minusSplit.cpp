7c5a1b713757cabb3e632adf2ef974d4e8b4d5ac
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-01-07 09:51:27 +0600

    minusSplit

---
+++ plus/minusSplit.cpp
@@ -0,0 +1,52 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define Mehedi_hasan() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int p = 0, m = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '+')
+            p++;
+        else
+            m++;
+    }
+    cout << abs(p - m) << endl;
+}
+int32_t main()
+{
+    Mehedi_hasan();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    r0;
+}
