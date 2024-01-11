32c2e838fac1793a8eb78af57d2d8ee7833bfdd7
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-01-11 10:36:54 +0600

    Helmets in light

--- HelmetsInNightLight.cpp
+++ HelmetsInNightLight.cpp
@@ -25,6 +25,26 @@ typedef pair<ll, ll> pll;
 typedef map<int, int> mii;
 void solve()
 {
+    int n, p;
+    cin >> n >> p;
+    int arr[n], brr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int ans = p;
+    vector<pair<int, int>> vec;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> brr[i];
+        vec.push_back({min(p, brr[i]), arr[i]});
+    }
+    sort(vec.begin(), vec.end());
+    int person = n - 1;
+    for (int i = 0; i < n - 1; i++)
+    {
+        ans += min(vec[i].second, person) * vec[i].first;
+        person -= min(person, vec[i].second);
+    }
+    cout << ans << endl;
 }
 int32_t main()
 {
