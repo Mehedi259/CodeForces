c542b668bbc0042f41da585a618a1f43a2b5b3e9
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-01-21 14:28:21 +0600

    coridor

---
+++ CorridororThereandBackAgain.cpp
@@ -0,0 +1,52 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define r0 return 0
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REV(i, n) for (int i = n - 1; i >= 0; i--)
+#define Mehedi_hasan() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int n,a,b,k=INT_MAX;
+    cin >> n;
+    vector<pair<int, int> >v;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        v.push_back({a,b});
+    }
+    for(int i = 0; i < (int)v.size(); i++)
+    {
+        k=min(v[i].first+(v[i].second-1)/2,k);
+    }
+    cout << k << endl;
+}
+int32_t main()
+{
+    Mehedi_hasan();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    r0;
+}
