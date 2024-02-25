fe567e47a5f763dbf1dc8a5f1acdb024b6345665
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-02-25 07:12:08 +0600

    sasha

---
+++ SashaandtheBeautifulArray.cpp
@@ -0,0 +1,47 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define Mehedi_hasan() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int n, s(0);
+    cin >> n;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(all(a));
+    for (int i = 1; i < n; i++)
+    {
+        s += a[i] - a[i - 1];
+    }
+    cout << s << endl;
+}
+int32_t main()
+{
+    Mehedi_hasan();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
