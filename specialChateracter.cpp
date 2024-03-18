bde23007f9255aef7fc38bc0f6d4e90f0da20c8f
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-03-18 07:03:27 +0600

    special caracter

---
+++ specialChateracter.cpp
@@ -0,0 +1,50 @@
+// Bismillahir Rahmanir Rohim
+//       Author
+// Mehedi Hasan Mridul
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define Mehedi_hasan() ios_base::sync_with_stdio(false), cin.tie(NULL)
+
+typedef long long ll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 2)
+        cout << "NO" << endl;
+    else
+    {
+        cout << "YES" << endl;
+        for (int i = 0; i < n / 2; i++)
+            for (int j = 0; j < 2; j++)
+            {
+                cout << (char)('A' + i);
+            }
+            cout << endl;
+    }
+}
+int32_t main()
+{
+    Mehedi_hasan();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
