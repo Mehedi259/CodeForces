56ce369fa4386ed4b7c645e94b4fdc1c71ddec0f
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-06-30 17:37:58 +0600

    Catch the Coin

---
+++ CatchTheCoin.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve()
+{
+    int x,y;
+    cin >> x >> y;
+    if(y >= -1) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+int32_t main(){int t;cin >> t;while(t--){solve();}return 0;}
