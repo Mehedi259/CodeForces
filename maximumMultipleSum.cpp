e01c04b5917d0e5028dcb1959af5c915def646a3
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-06-30 14:24:05 +0600

    maximumMultiple sum

---
+++ maximumMultipleSum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve()
+{
+    int n;
+    cin >> n;
+    if(n ==2 || n==3)
+    {
+        cout << n << endl;
+    }
+    else cout << 2 << endl;
+}
+int32_t main(){int t;cin >> t;while(t--){solve();}return 0;}
