fdfdf8c787ed56bbc28f9da17f230f186440ff3d
Mehedi Hasan Mridul <mmridul207@gmail.com>
2024-07-01 08:17:03 +0600

    problem 4

--- problem4.cpp
+++ problem4.cpp
@@ -1,35 +1,60 @@
 #include <bits/stdc++.h>
-#define A(a) (a).begin , (a).end()
-#define K first 
-#define V second 
-#define M 1000000007
-#define N 5010 
 using namespace std;
-ll dp[N][N];
-ll ct[N];
 
-#define DP dp[i][asc]
-ll r(ll i , ll asc){
-    if(asc<0) return -2;
-    if(!i) return asc ? -2 : 0;
-    if(~DP) return DP;
-    if(ct[i]) DP = r(i-1 , asc-1); else DP = r(i-1 , asc);
-    if(r(i-1,asc)!= -2 && r(i-1 , asc ) + ct[i] <= asc){
-        if(DP == -2 || r(i-1 , asc ) + ct[i] < DP) DP = r(i-1 , asc )+ ct[i];
+#define MAX_SIZE 5010
+using ll = long long;
+
+ll dp[MAX_SIZE][MAX_SIZE];
+ll frequency[MAX_SIZE];
+
+#define DP dp[index][ascensions]
+
+ll calculateMinOperations(ll index, ll ascensions) {
+    if (ascensions < 0)
+        return -2;
+    if (index == 0)
+        return ascensions ? -2 : 0;
+    if (~DP)
+        return DP;
+
+    if (frequency[index])
+        DP = calculateMinOperations(index - 1, ascensions - 1);
+    else
+        DP = calculateMinOperations(index - 1, ascensions);
+
+    if (calculateMinOperations(index - 1, ascensions) != -2 && calculateMinOperations(index - 1, ascensions) + frequency[index] <= ascensions) {
+        if (DP == -2 || calculateMinOperations(index - 1, ascensions) + frequency[index] < DP) {
+            DP = calculateMinOperations(index - 1, ascensions) + frequency[index];
+        }
     }
+
     return DP;
-    
 }
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    G(tc) while(tcc--){
-        G(n) F(i , 0 , n+1 ) fill_n(dp[i] , n+1 , -1);
-        F(i,0,n){ G(x) ct[x]++; }
-        ll ans = n ;
-        FD(i,n , -1) if(r(n,i) != -2 ) ans = i;
-        cout<<ans<<endl;
-        fill_n(ct,n+1,0ll);
-}
+int main() {
+    int testCases;
+    cin >> testCases;
+
+    while (testCases--) {
+        ll numElements;
+        cin >> numElements;
+
+        for (int i = 0; i <= numElements; i++)
+            fill_n(dp[i], numElements + 1, -1);
+
+        for (int i = 0; i < numElements; i++) {
+            ll value;
+            cin >> value;
+            frequency[value]++;
+        }
+
+        ll result = numElements;
+        for (int i = numElements; i >= 0; i--)
+            if (calculateMinOperations(numElements, i) != -2)
+                result = i;
+
+        cout << result << '\n';
+
+        fill_n(frequency, numElements + 1, 0ll);
+    }
 }
